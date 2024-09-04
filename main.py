6f3b0a60630f03c0dbcd563961177f00fdff037c
thiagoanalytics <thiagoromualdo.nascimento@gmail.com>
2024-09-04 08:31:26 -0300

    feat: script inicial finalizado

---
+++ main.py
@@ -0,0 +1,55 @@
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+import pandas as pd
+import json
+import os
+from datetime import datetime
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = "17DLqVCU8MPu0TiqKPCn8pTUQIkas8iwiv8gAmdDWGkk"
+SAMPLE_RANGE_NAME = "BD_TON20240903"
+
+
+def main():
+    creds = None
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_secret.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('sheets', 'v4', credentials=creds)
+   
+
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
+    df = pd.DataFrame(values[1:], columns=values[0])
+
+    # Salva os dados em um arquivo CSV
+    df.to_csv('dados.csv', index=False, encoding='utf-8-sig')
+
+
+if __name__ == "__main__":
+  main()
